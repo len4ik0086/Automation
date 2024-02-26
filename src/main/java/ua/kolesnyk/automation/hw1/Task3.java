94a564773a3fadb1f14eb758e4a21e4ae5722353
Olena Kolesnyk <len4ik0086@gmail.com>
2024-02-26 23:00:32 +0100

    HW1 for check

---
+++ src/main/java/ua/kolesnyk/automation/hw1/Task3.java
@@ -0,0 +1,32 @@
+package ua.kolesnyk.automation.hw1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Task3 {
+
+    private WebDriver webDriver;
+
+    @BeforeMethod
+    public void initDriver() {
+        webDriver = new ChromeDriver();
+    }
+
+    @AfterMethod()
+    public void destroy() {
+        webDriver.quit();
+    }
+
+    @Test
+    public void task3() {
+        webDriver.get("https://rozetka.com.ua/");
+        WebElement image = webDriver.findElement(By.xpath("//input[@placeholder='Я ищу...']"));
+        Assert.assertTrue(image.isDisplayed(), "Element is not opened successfully");
+    }
+}
